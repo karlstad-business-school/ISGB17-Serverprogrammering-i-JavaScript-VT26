5ab3f9c2acd8828a51cda6f245a7a2051ffd143e
Pierre Sjöberg <pierre.sjoberg@kau.se>
2026-04-24 11:46:03 +0200

    F2 slutkod

--- F2/Kodexempel-2/app.js
+++ F2/Kodexempel-2/app.js
@@ -7,3 +7,28 @@
     beräkning.
 
 */
+
+const fs = require('fs').promises;
+
+async function bearbetaFil() {
+    console.log('[1] Påbörjar fil-läsning');
+
+    let innehall = await fs.readFile('stor-fil.txt', 'utf-8');
+    console.log('[2] Filläsning KLAR');
+    console.log('[3] Påbörjar tung beräkning');
+
+    let antalA = 0;
+    for(let i=0;i<500;i++) {
+        for(let j=0; j<innehall.length;j++) {
+            if(innehall[j]==='a' || innehall[j]==='A') {
+                antalA++;
+            }
+        }
+    }
+    console.log('[4] Beräkning färdig, hittade ' + antalA + ' st a eller A');
+
+}
+
+bearbetaFil();
+
+console.log('tadaaaaa');
