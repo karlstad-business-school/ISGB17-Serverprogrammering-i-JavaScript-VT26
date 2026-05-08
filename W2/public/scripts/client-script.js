e920d87f9eda34878345541d967e15f79376f466
Pierre Sjöberg <pierre.sjoberg@kau.se>
2026-05-08 15:58:01 +0200

    W2 slutkod

--- W2/public/scripts/client-script.js
+++ W2/public/scripts/client-script.js
@@ -1,1 +1,21 @@
 'use strict';
+const socket = io();
+
+let knapps = document.querySelectorAll('.birdbutton');
+
+for(let i = 0 ; i< knapps.length; i++) {
+    knapps[i].addEventListener('click', ettnamn);
+}
+
+function ettnamn(e) {
+    e.preventDefault();
+    socket.emit('newBackground', e.target.getAttribute('data-birdid'));
+
+}
+
+socket.on('byt', function(data) {
+    document.querySelector('body').style.backgroundImage = 'url(public/images/' + data.bildnr + '.jpg)';
+    document.querySelector('.lead').textContent = 'Bilden uppdaterades av ' + data.nick;
+
+
+});
--- W2/server.js
+++ W2/server.js
@@ -1,1 +1,61 @@
 'use strict';
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const {Server} = require('socket.io');
+const { parseCookies, getTimeStamp } = require('./my-module.js');
+
+const app = express();
+const port = 3002;
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/public', express.static(__dirname + '/public'));
+
+const httpServer = app.listen(port, function() {
+     console.log(`Servern lyssnar på http://localhost:${port}`);
+})
+
+const io = new Server(httpServer);
+
+
+app.get('/', (req, res) => {
+    console.log('hej');
+    
+    if (req.cookies.nickname) {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    } else {
+        res.sendFile(path.join(__dirname, 'loggain.html'));
+    }
+});
+
+app.post('/', (req,res)=> {
+
+    res.cookie('nickname', req.body.nickname);
+    res.redirect('/');
+
+
+});
+
+io.on('connection', function(socket) {
+    console.log('sdfs');
+
+    socket.on('newBackground', (data)=>{
+        
+         const cookieHeader = socket.handshake.headers.cookie || '';
+
+         let kakor = parseCookies(cookieHeader);
+        console.log('Fågel: ' + data + ': nickname är ' + kakor.nickname);
+     console.log(cookieHeader);
+
+        io.emit('byt', {'bildnr': data, 'nick': kakor.nickname});
+
+    });
+
+
+
+});
+
