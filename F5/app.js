44dbf7ac36d212b7a6253c67d7ed94945ad8a9dd
Pierre Sjöberg <pierre.sjoberg@kau.se>
2026-05-08 11:57:07 +0200

    F5 slutkod

--- F5/app.js
+++ F5/app.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const express = require('express');
+const app = express();
+const { Server } = require('socket.io');
+
+app.use('/silverfisk', express.static(__dirname + '/clientscripts'));
+
+const httpServer = app.listen(3002, function() {
+    console.log('Server igång!');
+});
+
+const io = new Server(httpServer);
+
+
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/favicon.ico', function(req,res) {
+    res.sendFile(__dirname + '/favicon.ico');
+});
+
+io.on('connection', function(socket) {
+
+    console.log('En klient har anslutit via socket');
+
+    socket.on('silver', function(data) {
+        //console.log('Tjo!');
+
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+
+        io.emit('color' , { 'red': r, 'green': g, 'blue': b });
+
+
+    });
+
+
+
+
+});
--- F5/clientscripts/clientscript.js
+++ F5/clientscripts/clientscript.js
@@ -1,1 +1,17 @@
 'use strict';
+const socket = io();
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.querySelector('.btn').addEventListener('click', buttonClicked);
+});
+
+function buttonClicked() {
+
+    socket.emit('silver', null);
+
+}
+
+socket.on('color', function(data) {
+    document.querySelector('body').style.backgroundColor = 'rgb(' + data.red + ',' + data.green + ',' + data.blue + ')';
+});
+
